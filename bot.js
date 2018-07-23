86df4ef217100ed318a8230e3705addfcf9b2597
Glitch (discordjs-bot) <none>
2018-07-23 18:21:08 +0000

    😅👷 Checkpoint ./bot.js:1423701/1640

--- bot.js
+++ bot.js
@@ -1,14 +1,36 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs");
 
-client.on("ready", () => {
-  console.log("I am ready!");
+const discord_token = process.env.TOKEN;
+const prefix = process.env.PREFIX;
+
+// This loop reads the /events/ folder and attaches each event file to the appropriate event.
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    let eventFunction = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
+    client.on(eventName, (...args) => eventFunction.run(client, ...args));
+  });
 });
 
-client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+client.on("message", message => {
+  if (message.author.bot) return;
+  if(message.content.indexOf(prefix) !== 0) return;
+
+  // This is the best way to define args. Trust me.
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  // The list of if/else is replaced with those simple 2 lines:
+  try {
+    let commandFile = require(`./commands/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
   }
 });
 
-client.login(process.env.TOKEN);
+client.login(discord_token);
