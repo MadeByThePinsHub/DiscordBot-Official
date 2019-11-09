909b4a74e22cce80c9cc001203acbf097146d225
Andrei Jiroh Halili <andreijiroheugeniohalili24680@gmail.com>
2019-11-09 13:00:10 +0000

    Deleted some mess and add some juicy things

--- bin/bootup.sh
+++
@@ -1,7 +0,0 @@
-# We use NPM to manage our packages and to save space in Glitch.
-
-# Installs the package dependencies first, as listed in our package.json (unless you updated it)
-npm install
-
-# If installation are successful, run the bot.
-node bot.js
